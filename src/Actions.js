3dc0570659b468094d24c3df0b76258ac35054f3
Malte W <wessel@rheinschafe.de>
2015-03-15 21:05:19 +0100

    Convert tabs to spaces

--- src/Actions.js
+++ src/Actions.js
@@ -2,19 +2,19 @@ import { EventEmitter } from 'eventemitter3';
 
 export default class Actions extends EventEmitter {
 
-	emit(event, ...args) {
-		if(typeof this[event] !== 'function') {
-			console.warn(
-				`${this.constructor.name} emitted \`${event}\`. ` + 
-				`This action is not implemented and can therefore not be dispatched.`
-			);
-		}
+    emit(event, ...args) {
+        if(typeof this[event] !== 'function') {
+            console.warn(
+                `${this.constructor.name} emitted \`${event}\`. ` + 
+                `This action is not implemented and can therefore not be dispatched.`
+            );
+        }
 
-		return super.emit(event, ...args);
-	}
+        return super.emit(event, ...args);
+    }
 
-	dispatch(...args) {
-		return this.emit(...args);
-	}
+    dispatch(...args) {
+        return this.emit(...args);
+    }
 
 }
